83b928d71570efedfaea0c83ae79de19214cb73e
Sumeyra Davran <sumeyra.ozgur91@gmail.com>
2022-08-15 16:45:05 +0300

    add routing support

--- src/routes/routesConfigs.tsx
+++ src/routes/routesConfigs.tsx
@@ -1,9 +1,9 @@
 import React, { Suspense } from 'react'
 
 // eslint-disable-next-line @typescript-eslint/promise-function-async
-const Home = React.lazy(() => import('pages/Home/Home'))
+const Home = React.lazy(() => import('pages/Home'))
 // eslint-disable-next-line @typescript-eslint/promise-function-async
-const Page = React.lazy(() => import('pages/Page/Page'))
+const Page = React.lazy(() => import('pages/Page'))
 
 export const routeConfigs = [
   {
